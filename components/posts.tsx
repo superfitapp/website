5578d3d7122abbf979e139b0c1c043062558ae79
Leo Kwan <leo@superfitapp.com>
2021-01-03 16:04:22 -0500

    add blog

---
+++ components/posts.tsx
@@ -0,0 +1,8 @@
+import GhostContentAPI from "@tryghost/content-api";
+
+// Create API instance with site credentials
+const api = new GhostContentAPI({
+  url: "https://leokwan.ghost.io",
+  key: "d21e2b259960f1bc9d7c07e8ff",
+  version: "v3",
+});
